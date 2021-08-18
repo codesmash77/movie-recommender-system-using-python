ff3fcaee29e5915276a8825e6afdec6fb2a644a5
codesmash77 <33595080+codesmash77@users.noreply.github.com>
2021-08-18 16:36:13 +0530

    removed bloated code

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,6 +1,4 @@
-Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 text = ["London Paris London","Paris Paris London"]
--- starter.py
+++ starter.py
@@ -1,6 +1,4 @@
-Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import pandas as pd
+import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
